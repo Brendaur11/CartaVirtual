7c15f39a7253e65fb233217ba29f4abe84a24250
Brenda Urquiza <burquiza@jla.local>
2025-09-12 16:12:23 -0300

    Commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom'; // 👈 Agregá esto
 import './index.css';
 import './App.css';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
       <App />
-    </HashRouter>
   </StrictMode>,
 );
